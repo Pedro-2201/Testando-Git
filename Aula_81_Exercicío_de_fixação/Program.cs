9dc2b39a16922492f3757ebbb752033515380cf8
Pedro Henrique <pedrohmxavier@gmail.com>
2020-04-01 16:05:07 -0300

    Tirando codigo inútil

--- Aula_81_Exercicío_de_fixação/Program.cs
+++ Aula_81_Exercicío_de_fixação/Program.cs
@@ -48,28 +48,7 @@ namespace Aula_81_Exercicío_de_fixação
                             if (l < lines - 1)
                             {
                                 Console.WriteLine("Down: " + mat[l + 1, c]);
-                            }/*
-                            if (mat[l, c] != null)
-                            {
-                                Console.WriteLine("Left: " + mat[l, c - 1]);
                             }
-                            if (mat[l + 1, c] != null)
-                            {
-                                Console.WriteLine("Right: " + mat[l, c + 1]);
-                            }
-                            if (mat[l, c - 1] != null)
-                            {
-                                Console.WriteLine("Up: " + mat[l - 1, c]);
-                            }
-                            if (mat[l, c + 1] != null)
-                            {
-                                Console.WriteLine("Down: " + mat[l, c]);
-                            }
-                            Console.WriteLine("Position: " + mat[l, c]);
-                            Console.WriteLine("Left: " + mat[(l - 1), c]);
-                            Console.WriteLine("Up: " mat[(l)]);
-                            Console.WriteLine("Right: " + mat[(l + 2), c]);
-                            */
                         }
                     }
                 }
@@ -78,35 +57,6 @@ namespace Aula_81_Exercicío_de_fixação
             {
                 Console.WriteLine("Number dosen't exist");
             }
-
-            /*Console.Write("Position: ");
-            string[] position = Console.ReadLine().Split(",");
-            int posit1 = int.Parse(position[0]);
-            int posit2 = int.Parse(position[1]);
-            if (mat[posit1, posit2] != null)
-            {
-                posit2 -= 1;
-                if (mat[posit1, posit2] != null)
-                {
-                    Console.WriteLine("Left: " + mat[posit1, posit2]);
-                }
-                posit2 += 2;
-                if (mat[posit1, posit2] != null)
-                {
-                    Console.WriteLine("Right: " + mat[posit1, posit2]);
-                }
-                posit2 -= 1;
-                posit1 -= 1;
-                if (mat[posit1, posit2] != null)
-                {
-                    Console.WriteLine("Up:" + mat[posit1, posit2]);
-                }
-                posit1 += 2;
-                if (mat[posit1, posit2] != null)
-                {
-                    Console.WriteLine("Down: " + mat[posit1, posit2]);
-                }
-            }*/
 
         }
     }
